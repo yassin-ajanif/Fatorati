414382e22c1ab5550a2e4f68def3ba59f6655d66
yassin-ajanif <y.ajanif@enjazdev.com>
2026-06-20 21:40:40 +0200

    feat: replace pagination controls in StockMainView with ListPaginationBar for improved code organization and maintainability

---
+++ Shared/Views/ListPaginationBar.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace GestionCommerciale.Shared.Views;
+
+public partial class ListPaginationBar : UserControl
+{
+    public ListPaginationBar() => InitializeComponent();
+}
